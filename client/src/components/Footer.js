447c8ebd73bc1cfedbe83b166796ee77b9d60f7a
Carl Berthold <Carl@space-time.tv>
2021-10-07 17:18:14 +0200

    last try

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <div>
             <footer>
+                <h1>test</h1>
             <p>created by Carl Berthold during the Bootcamp 2021</p>
            </footer>
         </div>
