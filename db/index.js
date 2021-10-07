db261503074dc45cce1dd1c64f750cd516acd8af
Carl Berthold <Carl@space-time.tv>
2021-10-07 17:48:31 +0200

    mongoDb

--- db/index.js
+++ db/index.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/myWishCar";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGODB_URI)
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
